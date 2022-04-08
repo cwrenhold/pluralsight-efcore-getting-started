666651cc7140f42d7b9a9f4ba091d4ef2757095c
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2022-04-08 14:57:31 +0100

    Added some basic example operations, e.g. remove/delete, updates, find by ID

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -7,6 +7,11 @@ _context.Database.EnsureCreated();
 
 getSamurais("Before Add");
 addSamuraiByName("Shimada", "Okamoto", "Kikuchio", "Hayashida");
+queryAggregates();
+retrieveAndUpdateSamurai();
+retrieveAndUpdateMultipleSamurai();
+multipleDatabaseOperations();
+retrieveAndDeleteSamurai();
 // getSamurais("After Add");
 
 void addSamuraiByName(params string[] names)
@@ -29,5 +34,52 @@ void getSamurais(string text)
     foreach (var samurai in samurais)
     {
         System.Console.WriteLine(samurai.Name);
+    }
+}
+
+void queryAggregates()
+{
+    var samurai = _context.Samurais.Find(2);
+}
+
+void retrieveAndUpdateSamurai()
+{
+    var samurai = _context.Samurais.FirstOrDefault();
+    
+    if (samurai is not null)
+    {
+        samurai.Name += "San";
+        _context.SaveChanges();
+    }
+}
+
+void retrieveAndUpdateMultipleSamurai()
+{
+    var samurai = _context.Samurais.Skip(1).Take(5).ToList();
+    samurai.ForEach(s => s.Name += "San");
+    _context.SaveChanges();
+}
+
+void multipleDatabaseOperations()
+{
+    var samurai = _context.Samurais.FirstOrDefault();
+
+    if (samurai is not null)
+    {
+        samurai.Name += "San";
+    }
+
+    _context.Add(new Samurai { Name = "Shino"});
+    _context.SaveChanges();
+}
+
+void retrieveAndDeleteSamurai()
+{
+    var samurai = _context.Samurais.Find(5);
+    
+    if (samurai is not null)
+    {
+        _context.Remove(samurai);
+        _context.SaveChanges();
     }
 }
