9b53df5f0a76f2250b81a26c93888b852de0d5f9
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2022-04-08 13:23:23 +0100

    Added many-to-many relationship example

--- SamuraiApp.Data/Migrations/20220408121405_manytomanysimple.Designer.cs
+++ SamuraiApp.Data/Migrations/20220408121405_manytomanysimple.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SamuraiApp.Data;
 
@@ -10,9 +11,10 @@ using SamuraiApp.Data;
 namespace SamuraiApp.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20220408121405_manytomanysimple")]
+    partial class manytomanysimple
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SamuraiApp.Domain/Battle.cs
@@ -0,0 +1,8 @@
+namespace SamuraiApp.Domain;
+
+public class Battle
+{
+    public int BattleId { get; set; }
+    public string Name { get; set; }
+    public List<Samurai> Samurais { get; set; } = new List<Samurai>();
+}
